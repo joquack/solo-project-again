5cbc51a258b0259795588abea068ea8683356ffc
Joel Cruz <joel.irvin@cruz-family.ws>
2022-06-29 23:08:11 -0700

    added the navigation bar to /photos

--- frontend/src/components/PhotosPage/index.js
+++ frontend/src/components/PhotosPage/index.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import {getAllPhotos} from '../../store/photos'
+import Navigation from '../Navigation';
+import * as sessionActions from "../../store/session";
+
 import './index.css'
 function PhotosPage () {
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
     const photos = Object.values(useSelector(state => state?.photos)).reverse()
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    }, [dispatch]);
 
     // const [allPhotos, setAllPhotos] = useState([])
     // const photos = Object.values(photosObj)
@@ -21,6 +29,7 @@ function PhotosPage () {
 
     return (
         <>
+        <Navigation isLoaded={isLoaded}/>
         <h2>All Photos</h2>
         <Link to='/create'>Create A New Photo</Link>
         <div>
