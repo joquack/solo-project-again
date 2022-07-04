e021267df5fffd8f1ab3cbe34e6e94d2892de41c
Joel Cruz <joel.irvin@cruz-family.ws>
2022-07-04 13:39:51 -0700

    finally got css grid

--- frontend/src/components/PhotosPage/index.js
+++ frontend/src/components/PhotosPage/index.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import {getAllPhotos} from '../../store/photos'
+import { getAllPhotos } from '../../store/photos'
 import Navigation from '../Navigation';
 import * as sessionActions from "../../store/session";
 import './index.css'
 
 import './index.css'
-function PhotosPage () {
+function PhotosPage() {
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
     const photos = Object.values(useSelector(state => state?.photos)).reverse()
@@ -30,15 +30,17 @@ function PhotosPage () {
 
     return (
         <>
-        <Navigation isLoaded={isLoaded}/>
+        <Navigation isLoaded={isLoaded} />
         <h2>All Photos</h2>
         <Link to='/create'>Create A New Photo</Link>
-        <div>
-            <ul>
+        <div className='container'>
             {photos && photos.map(img => {
-                return <li key={img.id}><Link to={`/photos/${img.id}`}><img className='photoimg' src={`${img.source}`}></img></Link></li>
+                return <div key={img.id} className='gallery-container'>
+                    <div className='gallery-item'>
+                        <Link to={`/photos/${img.id}`}><img className='image' src={`${img.source}`}></img></Link>
+                    </div>
+                </div>
             })}
-            </ul>
         </div>
         </>
     )
